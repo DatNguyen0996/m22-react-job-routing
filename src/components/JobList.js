c3007fea428fbc89589eaf52974102b76fc3fa64
Dat_Nguyen <ngxudat@gmail.com>
2022-12-30 13:27:27 +0700

    fix

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -2,6 +2,7 @@ import JobCard from "../components/JobCard";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import { useParams } from "react-router-dom";
 
 function JobList({ jobList, islogined, setIslogined }) {
@@ -12,37 +13,39 @@ function JobList({ jobList, islogined, setIslogined }) {
 
   return (
     <Typography sx={{ justifyContent: "center" }} component="div">
-      <Grid
-        container
-        spacing={2}
-        sx={{
-          direction: "row",
-          justifyContent: "center",
-          mt: 5,
-          paddingLeft: { xs: 5, sm: 20, md: 30, lg: 50 },
-          paddingRight: { xs: 5, sm: 20, md: 30, lg: 50 },
-        }}
-      >
-        {jobList.slice(pages * 5 - 5, pages * 5).map((job) => (
-          <Box
-            key={job.id}
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              "& > :not(style)": {
-                m: 1,
-                maxWidth: 300,
-              },
-            }}
-          >
-            <JobCard
-              job={job}
-              islogined={islogined}
-              setIslogined={setIslogined}
-            />
-          </Box>
-        ))}
-      </Grid>
+      <Container sx={{ p: 3 }} maxWidth="lg">
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            direction: "row",
+            justifyContent: "center",
+            mt: 5,
+            // paddingLeft: { xs: 5, sm: 20, md: 30, lg: 50 },
+            // paddingRight: { xs: 5, sm: 20, md: 30, lg: 50 },
+          }}
+        >
+          {jobList.slice(pages * 5 - 5, pages * 5).map((job) => (
+            <Box
+              key={job.id}
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                "& > :not(style)": {
+                  m: 1,
+                  maxWidth: 300,
+                },
+              }}
+            >
+              <JobCard
+                job={job}
+                islogined={islogined}
+                setIslogined={setIslogined}
+              />
+            </Box>
+          ))}
+        </Grid>
+      </Container>
     </Typography>
   );
 }
